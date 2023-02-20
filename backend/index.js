a02f7c79dc475a27db88202f8589878852385373
Corentin Ancel <corentin.ancel@edu.devinci.fr>
2023-02-20 10:40:20 +0100

    ADD: index.js file

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+// Create a new HTTP server using the express app
+const server = require("http").createServer(app);
+
+// Create a new Socket.IO server using the HTTP server
+const io = require("socket.io")(server);
+
+// Define a route handler for the default home page
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Start the server and listen for incoming connections
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+
+// Listen for incoming socket connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Listen for incoming chat messages
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  // Listen for disconnect events
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
