a546341f20561893b5c6830196e72b888cfdce29
Corentin Ancel <corentin.ancel@edu.devinci.fr>
2023-02-20 14:58:05 +0100

    Merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react";
 import { GamePage } from "./pages/Game";
 import Game from "./classes/game.js";
@@ -6,8 +5,6 @@ import Character from "./classes/character.js";
 import Incendio from "./classes/Spells/Incendio";
 import Reparo from "./classes/Spells/Reparo";
 
-=======
->>>>>>> main
 function App() {
   let character1 = new Character("John", "Doe", 100, 100, 10, [
     new Incendio(),
@@ -33,13 +30,9 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
     <div className="App">
       <GamePage />
     </div>
-=======
-    <div className="App"></div>
->>>>>>> main
   );
 }
 
