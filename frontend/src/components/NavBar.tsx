befd4729956dd4f299f2053e10758c0428410202
Corentin Ancel <corentin.ancel@edu.devinci.fr>
2023-02-21 14:03:56 +0100

    Fix

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -15,6 +15,9 @@ const NavBar = () => {
         <li>
           <Link to="/login">Login</Link>
         </li>
+        <li>
+          <Link to="/game">Game</Link>
+        </li>
       </ul>
     </nav>
   );
