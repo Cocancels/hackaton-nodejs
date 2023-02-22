6e66c7d9fe79c979d1dbe2d62a0bebfd7fc3be92
Corentin Ancel <corentin.ancel@edu.devinci.fr>
2023-02-22 13:50:19 +0100

    FIX: Bugs

--- frontend/src/components/Character/Character.tsx
+++ frontend/src/components/Character/Character.tsx
@@ -7,18 +7,12 @@ import { StatusBar } from "./StatusBar/StatusBar";
 
 interface CharacterProps {
   character: CharacterData;
-  isCurrentPlayer: boolean;
-  onSpellClick: (id: number) => void;
 }
 
 const CharacterComponent = (props: CharacterProps) => {
-  const { character, isCurrentPlayer, onSpellClick } = props;
+  const { character } = props;
   const defaultImage =
     "https://3.bp.blogspot.com/-14UwUPZwa48/V1N5VPAv03I/AAAAAAAAFhk/QABZnaexs8Yv8tidLAqv-Sh8nI72I4dzgCLcB/s1600/Harry%252BPotter%252Brender1.png";
-
-  const handleSpellUse = (id: number) => {
-    onSpellClick(id);
-  };
 
   return (
     <div className="character">
@@ -31,19 +25,6 @@ const CharacterComponent = (props: CharacterProps) => {
         <ManaBar mana={character.mana} maxMana={character.maxMana} />
         <StatusBar status={character.status} />
       </div>
-      {character.spells &&
-        isCurrentPlayer &&
-        character.spells.map((spell, index) => {
-          return (
-            <Button
-              key={`index-${index}`}
-              onClick={() => handleSpellUse(spell.id)}
-              className={`${spell.type}-button`}
-              label={spell.name}
-              spell={spell}
-            />
-          );
-        })}
     </div>
   );
 };
