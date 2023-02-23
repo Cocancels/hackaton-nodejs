5fd2d5379300533bed32d0aacd6d4fcd4d283785
Corentin Ancel <corentin.ancel@edu.devinci.fr>
2023-02-23 13:47:46 +0100

    REM: All files

--- frontend/src/interfaces/Message.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "./User";
-import { Room } from "./Room";
-
-export interface Message {
-  content: string;
-  user: User | undefined;
-  room: Room | undefined;
-  createdAt: Date;
-}
--- frontend/src/interfaces/Spell.ts
+++
@@ -1,8 +0,0 @@
-export interface Spell {
-  id: number;
-  name: string;
-  description: string;
-  type: string;
-  cost: number;
-  power: number;
-}
