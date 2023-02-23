cc5f54899e42ccb9b32fddfe487ae0af339dd08e
Kélian <kelian.raoult_severac@edu.devinci.fr>
2023-02-23 11:26:02 +0100

    Update app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,7 +13,6 @@ function App() {
 
         <Routes>
           <Route path="/" element={<LoginForm />} />
-          <Route path="/character" element={<CreateUserForm />} />
           <Route path="/game" element={<GamePage />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/character" element={<CreateUserForm />} />
